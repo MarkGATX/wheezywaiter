09206567b220e52644415667c47cf744d305da34
Mark Gardner <themarkgardner@gmail.com>
2023-01-06 15:40:03 -0600

    removed lorem text from header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,6 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import { Button } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
@@ -110,74 +108,8 @@ export default function Header({ currentPage, handlePageChange, props }) {
             </Box>
             
         </Box>
-        <Container sx={{ position: 'relative' }}>
-        <Box sx={{ my: 2 }}>
-            {[...new Array(24)]
-               .map(
-                   () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-               )
-               .join('\n')}
-       </Box>
-   </Container>
+        
    </>
-
-        //         <AppBar elevation={trigger ? 4 : 0} sx={{ position: 'sticky' }} >
-        //             <Grid2 container sx={{
-        //                 justifyContent:'space-between',
-        //                 alignItems:'center',
-        //             }}>
-        //                 <Grid2 xs={12} sm={4} md={6}>
-        //                     <Toolbar sx={{
-        //                         height: trigger ? 160 : 300,
-        //                         transition: trigger ? "0.3s ease-in" : "0.3s ease-out",
-        //                         flexGrow: 1,
-        //                     }}>
-        //                         <img src={wheezyWaiterPic} style={{ height: '100%' }} alt='Wheezy Waiter' />
-        //                         <Typography variant="h4" style={{ textAlign: 'center' }} color='primary.contrastText' component="h1">
-        //                             WHEEZY WAITER
-        //                             <Typography variant="h6" style={{ textAlign: 'center' }} component="h6">
-        //                                 I make videos.
-        //                             </Typography>
-        //                         </Typography>
-
-
-
-        //                     </Toolbar>
-        //                 </Grid2>
-        //                 <Grid2 xs={12} sm={4} md={5}>
-        //                     <Box sx={{ display: { sm: 'block' }, display: 'flex', justifyContent:'flex-end' }}>
-
-        //                         <Button variant='outlined' sx={{mr:2}}>
-        //                             Videos
-        //                         </Button >
-        //                         <Button variant='contained' color='primary' sx={{mr:2}}>
-        //                             Blogs
-        //                         </Button>
-        //                         <Button variant='contained' sx={{mr:2}}>
-        //                             Contact
-        //                         </Button>
-
-        //                     </Box>
-        //                 </Grid2>
-        //             </Grid2>
-        //         </AppBar>
-        //         <Container sx={{ position: 'relative' }}>
-        //             <Box sx={{ my: 2 }}>
-        //                 {[...new Array(24)]
-        //                     .map(
-        //                         () => `Cras mattis consectetur purus sit amet fermentum.
-        // Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-        // Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-        // Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
-        //                     )
-        //                     .join('\n')}
-        //             </Box>
-        //         </Container>
-
-        //     </>
     );
 
 }
