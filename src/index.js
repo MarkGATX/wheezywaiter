619cbccb57685b3c8d46af7bd34634cca086343c
Mark Gardner <themarkgardner@gmail.com>
2023-02-06 22:10:13 -0600

    removed unneeded commented out code

--- src/index.js
+++ src/index.js
@@ -8,12 +8,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={wheezyTheme}>
-      <CssBaseline /> */}
-      {/* <Paper> */}
+
         <App />
-      {/* </Paper> */}
-    {/* </ThemeProvider> */}
+
   </React.StrictMode>
 );
 
